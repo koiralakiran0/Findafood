8a4136a1f4ba8d075176873b2957e120baeba4ba
koiralakiran0 <kkoirala@uncc.edu>
2017-12-02 14:39:47 -0500

    firebase

--- app/src/main/java/com/example/kiran/recipe4155/ActivitySignup.java
+++ app/src/main/java/com/example/kiran/recipe4155/ActivitySignup.java
@@ -1,16 +1,42 @@
 package com.example.kiran.recipe4155;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class ActivitySignup extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText fName;
+    private EditText lName;
+    private EditText email;
+    private EditText password;
+    private EditText passRepeat;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        mAuth = FirebaseAuth.getInstance();
+        fName = (EditText) findViewById(R.id.editText_firstname);
+        lName = (EditText) findViewById(R.id.editText_lastName);
+        email = (EditText) findViewById(R.id.editText_email);
+        password = (EditText) findViewById(R.id.editText_password);
+        passRepeat = (EditText) findViewById(R.id.editText_repeatPass);
 
         findViewById(R.id.button_cancel).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,9 +48,64 @@ public class ActivitySignup extends AppCompatActivity {
         findViewById(R.id.button_signup).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(ActivitySignup.this, RecipeActivity.class);
-                startActivity(intent);
+                signUp();
+                //Intent intent = new Intent(ActivitySignup.this, RecipeActivity.class);
+                //startActivity(intent);
             }
         });
+
+    }
+
+    private void signUp() {
+        final String fn = fName.getText().toString();
+        final String ln = lName.getText().toString();
+        String em = email.getText().toString();
+        String pass = password.getText().toString();
+        String passRe = passRepeat.getText().toString();
+
+        if (validateEmail(em) && validatePassword(pass, passRe)){
+            //CREATE USER IF THE EMAIL AND PASSWORD ARE VALID
+            mAuth.createUserWithEmailAndPassword(em, pass)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(ActivitySignup.this, task.getException().toString(), Toast.LENGTH_LONG).show();
+                            }
+                            else {
+                                Toast.makeText(ActivitySignup.this, "User Successfully Signed Up",
+                                        Toast.LENGTH_SHORT).show();
+                                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                                if (user != null) {
+                                    // Name, email address, and profile photo Url
+                                    UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                            .setDisplayName(fn + " " + ln)
+                                            .build();
+                                    user.updateProfile(profileUpdates);
+
+                                    Log.d("demo", user.getDisplayName() + " " + user.getEmail() + " " + user.getUid());
+                                }
+                                finish();
+                            }
+
+                        }
+                    });
+        }
+    }
+
+    private boolean validatePassword(String pass, String passRe) {
+        if (pass.equals(passRe)){
+            return true;
+        }
+        return false;
+    }
+
+    private boolean validateEmail(String em) {
+        if (em == null) {
+            return false;
+        } else {
+            return android.util.Patterns.EMAIL_ADDRESS.matcher(em).matches();
+        }
+
     }
 }
--- app/src/main/java/com/example/kiran/recipe4155/MainActivity.java
+++ app/src/main/java/com/example/kiran/recipe4155/MainActivity.java
@@ -1,22 +1,54 @@
 package com.example.kiran.recipe4155;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private EditText edit_email, editPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        edit_email = (EditText) findViewById(R.id.editText_email);
+        editPassword = (EditText) findViewById(R.id.editText_password);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Toast.makeText(MainActivity.this, user.getDisplayName() + " Logged In", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(MainActivity.this, IngredientsInput.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Enter Login or Click Sign Up", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
         findViewById(R.id.button_login).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, RecipeActivity.class);
-                startActivity(intent);
+                login();
             }
         });
 
@@ -28,4 +60,41 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void login() {
+        String email = edit_email.getText().toString();
+        String password = editPassword.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, task.getException().toString(),
+                                    Toast.LENGTH_SHORT).show();
+                        } else{
+
+                        }
+
+                    }
+                });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.signOut();
+            mAuth.removeAuthStateListener(mAuthListener);
+
+        }
+
+    }
+
 }
